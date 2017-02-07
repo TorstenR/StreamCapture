3daf09de3fdf01fc6610a2b347168a1d514e3f70
mawilkie <mawilkie@microsoft.com>
2017-02-07 13:55:42 -0800

    Updated channelhistory to use the thread safe version of dictionary

--- ChannelHistoryInfo.cs
+++ ChannelHistoryInfo.cs
@@ -1,5 +1,5 @@
 using System;
-using System.Collections.Generic;
+using System.Collections.Concurrent;
 
 namespace StreamCapture
 {
@@ -8,7 +8,7 @@ namespace StreamCapture
         public string channel { get; set; }
         public double hoursRecorded { get; set; }
         public long recordingsAttempted { get; set; }
-        public Dictionary<string,long> serverSpeed { get; set; }
+        public ConcurrentDictionary<string,long> serverSpeed { get; set; }
         public DateTime lastAttempt { get; set; }
         public DateTime lastSuccess { get; set; }
         public bool activeFlag { get; set; }
